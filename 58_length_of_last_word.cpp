0388bc7f7c9709f37bc2b29f4b2284d533cd2f1f
Navya Shukla <navyaa.shukla2812@gmail.com>
2026-08-22 19:31:14 +0530

    Add 58_length_of_last_word.cpp - LeetCode Problem 58

---
+++ 58_length_of_last_word.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        reverse(s.begin(),s.end());
+        int i=0;
+        while(i<s.size()&&s[i]==' '){
+            i++;
+        }
+        int count=0;
+        while(i<s.size()&&s[i]!=' '){
+            count++;
+            i++;
+        }
+        return count;
+    }
+};
