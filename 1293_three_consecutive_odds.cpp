f91904729f2bea1147aee6af406325fecb4e367d
Navya Shukla <navyaa.shukla2812@gmail.com>
2026-09-17 23:51:39 +0530

    Add 1293_three_consecutive_odds.cpp - LeetCode Problem 1293

---
+++ 1293_three_consecutive_odds.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+      int i=0;
+      int j=1;
+      int k=2;
+      while(k<arr.size()){
+        if(arr[i]%2!=0&&arr[j]%2!=0&&arr[k]%2!=0){
+            return true;
+        }
+        i++;
+        j++;
+        k++;
+      }
+      return false;  
+    }
+};
